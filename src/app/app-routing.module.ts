d5f66ea5eee3804ef573dcd967afe883b2f8df9b
Anjali Dubey <anjalidubey9844@gmail.com>
2020-02-08 14:57:46 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { AsJewelerComponent } from './as-jeweler/as-jeweler.component';
-import { AsCitizenComponent } from './as-citizen/as-citizen.component';
 import { LoginComponent } from './login/login.component';
 import { FormsComponent } from './forms/forms.component';
 import { PasswordChangeComponent } from './password-change/password-change.component';
@@ -11,7 +10,6 @@ import { CreateViewComponent } from './create-view/create-view.component';
 import { ReadComponent } from './read/read.component';
 import { UpdateComponent } from './update/update.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
-import { PasswordComponent } from './password/password.component';
 import { OrnamentsComponent } from './ornaments/ornaments.component';
 import { RingsComponent } from './ornaments/rings/rings.component';
 import { NeckComponent } from './ornaments/neck/neck.component';
@@ -20,7 +18,6 @@ import { NeckComponent } from './ornaments/neck/neck.component';
 const routes: Routes = [
   { path: 'user', component: UserComponent},
   { path: 'jeweler', component: AsJewelerComponent},
-  { path: 'citizen', component: AsCitizenComponent},
   { path: 'login', component: LoginComponent},
   { path: 'changepassword', component: PasswordChangeComponent},
   { path: 'customer', component: CustomerComponent},
@@ -29,7 +26,6 @@ const routes: Routes = [
   { path: 'read', component: ReadComponent},
   { path: 'forms', component: FormsComponent},
   { path: 'dropdown', component: DropdownComponent},
-  { path: 'password' , component: PasswordComponent },
   { path: 'rings' , component: OrnamentsComponent,
     children: [{ path : '' , component : RingsComponent }]
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { UserService } from './shared/user.service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UserComponent } from './user/user.component';
 import { AsJewelerComponent } from './as-jeweler/as-jeweler.component';
-import { AsCitizenComponent } from './as-citizen/as-citizen.component';
 import { LoginComponent } from './login/login.component';
 import { FormsComponent } from './forms/forms.component';
 import { PasswordChangeComponent } from './password-change/password-change.component';
@@ -17,7 +16,6 @@ import { CreateViewComponent } from './create-view/create-view.component';
 import { ReadComponent } from './read/read.component';
 import { UpdateComponent } from './update/update.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
-import { PasswordComponent } from './password/password.component';
 import { ToastrModule } from 'ngx-toastr';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,7 +30,6 @@ import { NeckComponent } from './ornaments/neck/neck.component';
     NavbarComponent,
     UserComponent,
     AsJewelerComponent,
-    AsCitizenComponent,
     LoginComponent,
     FormsComponent,
     PasswordChangeComponent,
@@ -41,7 +38,6 @@ import { NeckComponent } from './ornaments/neck/neck.component';
     ReadComponent,
     UpdateComponent,
     DropdownComponent,
-    PasswordComponent,
     OrnamentsComponent,
     RingsComponent,
     NeckComponent
